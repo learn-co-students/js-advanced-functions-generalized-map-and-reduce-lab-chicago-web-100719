4b39116f1149f1f45ed8c55ac0cb70578de3c121
Daniel Ionita <danielionita88@yahoo.com>
2019-11-29 17:21:20 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your functions here
+const map = (array, funct) => {
+    let result = []
+    for (let i = 0; i< array.length; i++) {
+        result.push(funct(array[i]))
+    }
+    return result
+}
+
+const reduce = (array, funct, startingValue)=> {
+
+    let result = (!!startingValue) ? startingValue : array[0]
+    let i = (!!startingValue) ? 0 : 1
+
+    for (i ; i< array.length; i++){
+        result = funct(array[i], result)
+    }
+    return result
+}
