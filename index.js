99d82c5d1987b3bc1d5f80ad784cb8448e56abc7
github NikolaCzv <github nikola.raicic@hotmail.com>
2019-11-30 15:54:31 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your functions here
+function map (src, block) {
+    let arr = []
+    for (let i = 0; i < src.length; i++) {
+        let element = src[i]
+        arr.push(block(element))
+    }
+    return arr
+}
+
+function reduce(src, block, starting){
+    let arr = starting ? starting : src[0]
+    let i = starting ? 0 : 1
+  
+    for (; i < src.length; i++) {
+      arr = block(src[i], arr)
+    }
+  
+    return arr;
+}
