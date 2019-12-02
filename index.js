df5820767833a817d62240025e1b92a93c4e6f5f
bosmanpa <bsmn.ptrck@gmail.com>
2019-12-02 11:45:24 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your functions here
+function map(array, work){
+    let newArray = []
+    for(let i = 0; i < array.length; i++) {
+        let n = array[i]
+        newArray.push(work(n));
+    }
+    return newArray
+}
+function reduce(src, block, starting){
+    let arr = starting ? starting : src[0]
+    let i = starting ? 0 : 1
+
+    for (; i < src.length; i++) {
+      arr = block(src[i], arr)
+    }
+
+    return arr;
+} 
