71f440da97fda574f9293aff1a6dd7f6172483e1
Sarah Laloggia <54509406+slaloggia@users.noreply.github.com>
2019-12-01 20:22:08 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your functions here
+
+function map(srcArr, work) {
+    let newArr = []
+    
+    for(let i = 0; i < srcArr.length; i++) {
+        let n = srcArr[i]
+        newArr.push(work(n));
+    }
+    return newArr
+}
+
+function reduce(srcArr, cb, start= 0) {
+    let r = (!!start) ? start : srcArr[0]
+    let i = (!!start) ? 0 : 1
+
+    for(; i < srcArr.length; i++) {
+        r = cb(srcArr[i], r)
+    }
+    return r
+}
