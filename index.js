a469d733d26b8311c678c3a5345e51e223251110
Nathan Alex <nate745@gmail.com>
2019-12-02 14:13:13 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your functions here
+
+function map(sourceArray, x){
+    let newArray = []
+    for (let i=0; i < sourceArray.length; i++){
+        newArray.push(x(sourceArray[i]))
+    }
+    return newArray
+}
+
+function reduce(array, funct, startingPoint = 0) {
+    let marker = !!startingPoint ? startingPoint : array[0]
+    let i = !!startingPoint ? 0 : 1 
+
+    for (; i < array.length; i++) {
+        marker = funct(array[i], marker)
+    }
+
+    return marker;
+}
