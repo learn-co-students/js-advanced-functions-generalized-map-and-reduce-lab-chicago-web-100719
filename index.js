2208c84c50432d4ecf65604bb2cf14708ec1b220
NaveedG <nganjani@protonmail.com>
2020-02-01 17:09:29 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your functions here
+function map(arr, callback) {
+  let newArr = []
+  for (let x = 0; arr.length > x; x++) {
+    newArr.push(callback(arr[x]))
+  }
+  return newArr
+}
+
+function reduce(arr, callback, startingPoint=null) {
+  let total
+  let x = 0
+  if (startingPoint) {
+    total = startingPoint
+  } else {
+    total = arr[x]
+    x = 1
+  }
+  for (x; arr.length > x; x++) {
+    total = callback(total, arr[x])
+  }
+  return total
+}
